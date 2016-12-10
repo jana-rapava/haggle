1b6a5f0159123cf0cffa6b11957d2fb62ddad314
Jana Rapava <fermata7@gmail.com>
2016-12-10 15:22:30 +0100

    Problem: Generator function uses global constants.
    Solution: Make it more general.

--- Lloyd15.hs
+++ Lloyd15.hs
@@ -4,13 +4,6 @@ module Lloyd15 where
 
 import Control.Exception (assert)
 
-boardHeight = 4
-boardWidth = 4
--- TODO: make this configurable
-cellvals = "ABCD\
-        \EFGH\
-        \IJKL\
-        \MNO."
 adjacent = [[1,4],[0,2,5],[1,3,6],[2,7],
         [0,5,8], [1,4,6,9], [2,5,7,10], [3,6,11],
         [4,9,12], [5,8,10,13], [6,9,11,14], [7,10,15],
@@ -18,9 +11,9 @@ adjacent = [[1,4],[0,2,5],[1,3,6],[2,7],
 
 newtype Swap a = S ((Int,a), (Int,a)) deriving Show
 type Matrix a = [(Int, a)]
-type Board = Matrix Char
 
-generateBoard :: Board
-generateBoard = let size = boardHeight * boardWidth in
+generateBoard :: Int -> Int -> [a] -> Matrix a
+generateBoard boardHeight boardWidth cellvals =
+        let size = boardHeight * boardWidth in
                 assert (length cellvals == size)
                 zip [0..size-1] cellvals
