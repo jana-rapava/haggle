6fd42db3e2420328b5c09774be1646d69fa39a1e
Jana Rapava <fermata7@gmail.com>
2016-12-10 15:23:59 +0100

    Problem: Tests for more general uses of generator function are missing.
    Solution: Add them.

--- tests/TestLloyd15.hs
+++ tests/TestLloyd15.hs
@@ -3,16 +3,62 @@ module TestLloyd15 where
 import Test.HUnit
 import Lloyd15
 
+boardHeight1 = 4
+boardWidth1 = 4
+cellvals1 = "ABCD\
+        \EFGH\
+        \IJKL\
+        \MNO."
+
 testGenerateBoard1 :: Test
 testGenerateBoard1 = TestCase $ assertEqual ""
-        [0..boardHeight*boardWidth-1] (map fst $ generateBoard)
+        [0..boardHeight1*boardWidth1-1]
+        (map fst $ generateBoard boardHeight1 boardWidth1 cellvals1)
 
 testGenerateBoard2 :: Test
 testGenerateBoard2 = TestCase $ assertEqual ""
-        cellvals (map snd $ generateBoard)
+        cellvals1
+        (map snd $ generateBoard boardHeight1 boardWidth1 cellvals1)
+
+boardHeight2 = 2
+boardWidth2 = 7
+cellvals2 = [1, 2, 3, 4, 5, 6, 7,
+        8, 9, 10, 11, 12, 13, 0]
+
+testGenerateBoard3 :: Test
+testGenerateBoard3 = TestCase $ assertEqual ""
+        [0..boardHeight2*boardWidth2-1]
+        (map fst $ generateBoard boardHeight2 boardWidth2 cellvals2)
+
+testGenerateBoard4 :: Test
+testGenerateBoard4 = TestCase $ assertEqual ""
+        cellvals2
+        (map snd $ generateBoard boardHeight2 boardWidth2 cellvals2)
+
+boardHeight3 = 5
+boardWidth3 = 3
+cellvals3 = ["Lorem", "ipsum", "dolor",
+        "sit", "amet", "consectetur",
+        "adipiscing", "elit", "sed",
+        "do", "eiusmod", "tempor",
+        "incididunt", "ut",""]
+
+testGenerateBoard5 :: Test
+testGenerateBoard5 = TestCase $ assertEqual ""
+        [0..boardHeight3*boardWidth3-1]
+        (map fst $ generateBoard boardHeight3 boardWidth3 cellvals3)
+
+testGenerateBoard6 :: Test
+testGenerateBoard6 = TestCase $ assertEqual ""
+        cellvals3
+        (map snd $ generateBoard boardHeight3 boardWidth3 cellvals3)
 
 main :: IO Counts
 main = runTestTT $ TestList [
         testGenerateBoard1,
-        testGenerateBoard2
+        testGenerateBoard2,
+        testGenerateBoard3,
+        testGenerateBoard4,
+        testGenerateBoard5,
+        testGenerateBoard6
         ]
