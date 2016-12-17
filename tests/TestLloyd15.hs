a0e0bc520495a948648daa74e4bce5a521858dff
Jana Rapava <fermata7@gmail.com>
2016-12-17 14:23:42 +0100

    Problem: We lack tests for generateSwaps function.
    Solution: Add them.

--- tests/TestLloyd15.hs
+++ tests/TestLloyd15.hs
@@ -13,6 +13,9 @@ adjacent1 = [[1,4],[0,2,5],[1,3,6],[2,7],
         [0,5,8], [1,4,6,9], [2,5,7,10], [3,6,11],
         [4,9,12], [5,8,10,13], [6,9,11,14], [7,10,15],
         [8,13], [9,12,14], [10,13,15], [11, 14]]
+board1 = generateBoard boardHeight1 boardWidth1 cellvals1
+swaps1 = [S {posFrom = 15, sym = 'L', posTo = 11}, S {posFrom = 15, sym = 'O', posTo = 14}]
+blank1 = (=='.')
 
 testGenerateBoard1 :: Test
 testGenerateBoard1 = TestCase $ assertEqual ""
@@ -28,12 +31,19 @@ testComputeAdjacent1 :: Test
 testComputeAdjacent1 = TestCase $ assertEqual ""
         adjacent1 (computeAdjacent boardHeight1 boardWidth1)
 
+testGenerateSwaps1 :: Test
+testGenerateSwaps1 = TestCase $ assertEqual "chars"
+        swaps1 (generateSwaps boardHeight1 boardWidth1 board1 blank1 )
+
 boardHeight2 = 2
 boardWidth2 = 7
 cellvals2 = [1, 2, 3, 4, 5, 6, 7,
         8, 9, 10, 11, 12, 13, 0]
 adjacent2 = [[1,7],[0,2,8],[1,3,9],[2,4,10],[3,5,11],[4,6,12],[5,13],
         [0,8],[1,7,9],[2,8,10],[3,9,11],[4,10,12],[5,11,13],[6,12]]
+board2 = generateBoard boardHeight2 boardWidth2 cellvals2
+swaps2 = [S {posFrom = 13, sym = 7, posTo = 6}, S {posFrom = 13, sym = 13, posTo = 12}]
+blank2 = (==0)
 
 testGenerateBoard3 :: Test
 testGenerateBoard3 = TestCase $ assertEqual ""
@@ -49,6 +59,10 @@ testComputeAdjacent2 :: Test
 testComputeAdjacent2 = TestCase $ assertEqual ""
         adjacent2 (computeAdjacent boardHeight2 boardWidth2)
 
+testGenerateSwaps2 :: Test
+testGenerateSwaps2 = TestCase $ assertEqual "nums"
+        swaps2 (generateSwaps boardHeight2 boardWidth2 board2 blank2)
+
 boardHeight3 = 5
 boardWidth3 = 3
 cellvals3 = ["Lorem", "ipsum", "dolor",
@@ -61,6 +75,9 @@ adjacent3 = [[1,3],[0,2,4],[1,5],
         [3,7,9],[4,6,8,10],[5,7,11],
         [6,10,12],[7,9,11,13],[8,10,14],
         [9,13],[10,12,14],[11,13]]
+board3 = generateBoard boardHeight3 boardWidth3 cellvals3
+swaps3 = [S {posFrom = 14, sym = "tempor", posTo = 11}, S {posFrom = 14, sym = "ut", posTo = 13}]
+blank3 = (=="")
 
 testGenerateBoard5 :: Test
 testGenerateBoard5 = TestCase $ assertEqual ""
@@ -76,6 +93,10 @@ testComputeAdjacent3 :: Test
 testComputeAdjacent3 = TestCase $ assertEqual ""
         adjacent3 (computeAdjacent boardHeight3 boardWidth3)
 
+testGenerateSwaps3 :: Test
+testGenerateSwaps3 = TestCase $ assertEqual "strings"
+        swaps3 (generateSwaps boardHeight3 boardWidth3 board3 blank3)
+
 main :: IO Counts
 main = runTestTT $ TestList [
         testGenerateBoard1,
@@ -86,4 +107,8 @@ main = runTestTT $ TestList [
         testGenerateBoard6,
         testComputeAdjacent1,
         testComputeAdjacent2,
-        testComputeAdjacent3 ]
+        testComputeAdjacent3,
+        testGenerateSwaps1,
+        testGenerateSwaps2,
+        testGenerateSwaps3
+         ]
