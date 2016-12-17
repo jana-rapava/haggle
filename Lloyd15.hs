cbb685f5069ce222021583c886fe302fde53eecb
Jana Rapava <fermata7@gmail.com>
2016-12-17 14:20:10 +0100

    Problem: Type for Swap contains unneccessary information and too many parentheses.
    Solution: Use record notation.

--- Lloyd15.hs
+++ Lloyd15.hs
@@ -5,13 +5,12 @@ module Lloyd15 where
 import Control.Exception (assert)
 import Data.Maybe (catMaybes)
 
-adjacent = [[1,4],[0,2,5],[1,3,6],[2,7],
-        [0,5,8], [1,4,6,9], [2,5,7,10], [3,6,11],
-        [4,9,12], [5,8,10,13], [6,9,11,14], [7,10,15],
-        [8,13], [9,12,14], [10,13,15], [11, 14]]
-
-newtype Swap a = S ((Int,a), (Int,a)) deriving Show
 type Matrix a = [(Int, a)]
+data Swap a = S {
+                posFrom :: Int,
+                sym :: a,
+                posTo :: Int
+                } deriving (Eq, Show)
 
 generateBoard :: Int -> Int -> [a] -> Matrix a
 generateBoard boardHeight boardWidth cellvals =
