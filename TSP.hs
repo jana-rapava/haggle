ecc128a1339e12768ac118e04b47cdea0434acbb
Jana Rapava <fermata7@gmail.com>
2017-07-14 19:54:06 +0200

    Problem: We need to generate neighbours in the state space.
    Solution: Implement last functions which do that.
    
    Signed-off-by: Jana Rapava <fermata7@gmail.com>

--- TSP.hs
+++ TSP.hs
@@ -53,8 +53,10 @@ findExpanded :: (Eq a) => Graph a -> Maybe Int
 findExpanded g = elemIndex Expanded (map visited g)
 
 getVertex :: (Eq a) => Graph a -> a -> (Vertex a, Int)
-getVertex g l = (v, fromJust $ elemIndex v g)
-        where v = fromJust $ find ((==l) . label) g
+getVertex g l = (v, i)
+    where
+        v = fromJust $ find ((==l) . label) g
+        i = fromJust $ elemIndex v g
 
 getUnvisited :: (Eq a) => Graph a -> [a] -> [Int]
 getUnvisited g ls = map snd $ filter ((==Unvisited) . visited . fst) $ map (getVertex g) ls
@@ -74,3 +76,18 @@ generateMoves g = map mkMove $ zip (repeat e) moveTo
          e = fromJust (findExpanded g)
          moveTo = computeAdjacent g e
 
+-- find start vertex, mark it as processed
+-- find end vertex, mark it as expanded
+applyMove :: (Eq a) => Graph a -> Move a -> Graph a
+applyMove g m = l3 ++ (y1:l4) ++ (x1:l2)
+     where
+         pos1 = min (posFrom m) (posTo m)
+         pos2 = max (posFrom m) (posTo m)
+         (l1,x:l2) = splitAt pos2 g
+         (l3,y:l4) = splitAt pos1 l1
+         tmp = label (g !! (posFrom m))
+         x1 = if (label x == tmp) then mkVertex (label x, neighbours x, Processed) else mkVertex (label x, neighbours x, Expanded)
+         y1 = if (label y == tmp) then mkVertex (label y, neighbours y, Processed) else mkVertex (label y, neighbours y, Expanded)
+
+nextNeighbours :: (Eq a) => Graph a -> [Graph a]
+nextNeighbours g = map (applyMove g) (generateMoves g)
